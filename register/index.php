93fcef649238636d4576991f9a8b5b593dbe3712
Martin1701 <martinhusar21@gmail.com>
2023-12-16 19:03:58 +0100

    register validation rework+upgrade

--- register/index.php
+++ register/index.php
@@ -1,17 +1,49 @@
 <?php
 include_once "/home/husarma1/www/users.php";
-// get form data from request
-$fName = "";
-$lName = "";
-$login = "";
-$password = "";
-$password2 = "";
 
-$fNameNotice = "";
-$lNameNotice = "";
-$loginNotice = "";
-$passwordNotice = "";
-$password2Notice = "";
+class registerInput
+{
+    public $isPassword;     // if true use type="password"
+    public $value;          // pre-filled value (or user submitted)
+    public $notice;         // notice to display
+    public $name;           // name of the element
+    public $placeholder;    // input placeholder
+    public $valid;          // if login is valid or not
+    public $minLength;
+    public $maxLength;
+    function __construct($name, $placeholder,  $isPassword = false, $minLength = 0, $maxLength = 0)
+    {
+        $this->name = $name;
+        $this->placeholder = $placeholder;
+        $this->isPassword = $isPassword;
+        $this->minLength = $minLength;
+        $this->maxLength = $maxLength;
+        $this->valid = false;
+    }
+    // returns string that can be inserted directly into HTML
+    function get_html()
+    {
+        $element = "<input type=\"" . (($this->isPassword) ? "password" : "text") . "\" name=\"" . $this->name . "\"";
+        $element .= ($this->minLength) ? "minlength=\"" . $this->minLength . "\"" : "";
+        $element .= ($this->maxLength) ? "maxlength=\"" . $this->maxLength . "\"" : "";
+        if ($this->name == "login" && $this->valid) {
+            $element .= "class=\"correctInput\"";
+        } else {
+            $element .= (($this->notice) ? "class=\"incorrectInput\"" : "");
+        }
+
+        $element .= " value=\"" . htmlspecialchars($this->value, ENT_QUOTES) . "\"";
+        $element .= " placeholder=\"" . $this->placeholder . "\">";
+        $element .= "<p class=\"" . (($this->valid) ? "correctText" : "incorrectText") . "\">" . $this->notice . "</p>";
+        return $element;
+    }
+}
+
+$fName = new registerInput("fName", "First name*");
+$lName = new registerInput("lName", "Last name*");
+$login = new registerInput("login", "Login*", false, 4, 30);
+$password = new registerInput("password", "Password*", true, 8, 30);
+$password2 = new registerInput("password2", "Confirm password*", true); // only $password's length is validated
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Only way to register is through POST !
@@ -19,73 +51,72 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // TLDR: go through every register field, check if it is set, then if it has value
     // in case of login/password verify length and letter/number count
     // set user notice message if mistake found
+    // login is exception, special value valid is used, notice can be "Username valid."
 
     //* first name
-    if (isset($_POST["fName"]) && $_POST["fName"]) {
-        $fName = $_POST["fName"];
+    if (isset($_POST[$fName->name]) && $_POST[$fName->name]) {
+        $fName->value = $_POST[$fName->name];
     } else {
-        $fNameNotice = "This field is obligatory.";
+        $fName->notice = "This field is obligatory.";
     }
     //* last name
-    if (isset($_POST["lName"]) && $_POST["lName"]) {
-        $lName = $_POST["lName"];
+    if (isset($_POST[$lName->name]) && $_POST[$lName->name]) {
+        $lName->value = $_POST[$lName->name];
     } else {
-        $lNameNotice = "This field is obligatory.";
+        $lName->notice = "This field is obligatory.";
     }
     //* login
-    if (isset($_POST["login"]) && $_POST["login"]) {
-        $login = $_POST["login"];
-        $loginNotice = validate_input($login, 4, 30, "Login");
+    if (isset($_POST[$login->name]) && $_POST[$login->name]) {
+        $login->value = $_POST[$login->name];
+        $login->notice = validate_input($login->value, $login->minLength, $login->maxLength, "Login");
         // login is valid, check if the user already exists
-        if (!$loginNotice) {
-            if (getUserByLogin($login)) {
-                $loginNotice = "Username already in use.";
+        if (!$login->notice) {
+            if (getUserByLogin($login->value)) {
+                $login->notice = "Username already in use.";
+            } else {
+                $login->notice = "Username available.";
+                $login->valid = true;
             }
         }
     } else {
-        $loginNotice = "This field is obligatory.";
+        $login->notice = "This field is obligatory.";
     }
     //* password
-    if (isset($_POST["password"]) && $_POST["password"]) {
-        $password = $_POST["password"];
-        $passwordNotice = validate_input($password, 8, 30);
+    if (isset($_POST[$password->name]) && $_POST[$password->name]) {
+        $password->value = $_POST[$password->name];
+        $password->notice = validate_input($password->value, $password->minLength, $password->maxLength, "Password");
     } else {
-        $passwordNotice = "This field is obligatory.";
+        $password->notice = "This field is obligatory.";
     }
     //* retype password
-    if (isset($_POST["password2"]) && $_POST["password2"]) {
-        $password2 = $_POST["password2"];
-        if ($password != $password2) {
-            $password2Notice = "Passwords do not match.";
+    if (isset($_POST[$password2->name]) && $_POST[$password2->name]) {
+        $password2->value = $_POST[$password2->name];
+        if ($password->value != $password2->value) {
+            $password2->notice = "Passwords do not match.";
         }
     } else {
-        $password2Notice = "This field is obligatory.";
+        $password2->notice = "This field is obligatory.";
     }
 
     if (
-        !$fNameNotice &&
-        !$lNameNotice &&
-        !$loginNotice &&
-        !$passwordNotice &&
-        !$password2Notice
+        !$fName->notice &&
+        !$lName->notice &&
+        $login->valid &&
+        !$password->notice &&
+        !$password2->notice
     ) {
         // no notices so everything is valid
         session_start();
-        $h = password_hash($password, PASSWORD_DEFAULT);
-        $_SESSION["id"] = addUser($fName, $lName, $login, $h);
+        $h = password_hash($password->value, PASSWORD_DEFAULT);
+        $_SESSION["id"] = addUser($fName->value, $lName->value, $login->value, $h);
 
         // redirect user to main page
         header("location: ../");
     }
 }
-
-
-
-
-
 // returns 0 when valid !
 // else returns error message
-function validate_input($input, $minLength = 0, $maxLength = 30, $name = "")
+function validate_input($input, $minLength, $maxLength, $name = "")
 {
     // Check length
     $length = strlen($input);
@@ -108,6 +139,8 @@ function validate_input($input, $minLength = 0, $maxLength = 30, $name = "")
     // If all conditions are met, the password is valid
     return "";
 }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -125,65 +158,38 @@ function validate_input($input, $minLength = 0, $maxLength = 30, $name = "")
     include "../header.php";
     ?>
     <main class="main-page-container">
-        <br>
-        <section class="main-page-section">
+        <form action=<?php echo $_SERVER['PHP_SELF']; ?> method="post" id="form-register">
             <h1>Register</h1>
-            <form action=<?php echo $_SERVER['PHP_SELF']; ?> method="post" class="form-register">
-                <p>Please enter your login details</p>
-                <input type="text" class="form-text-input" name="fName" id="input-fname" value="<?php echo htmlspecialchars($fName, ENT_QUOTES) ?>" placeholder="First name*">
-                <?php if ($fNameNotice) {
-                    echo "<p class='notice'>" . $fNameNotice . "<p>";
-                }
-                ?>
-                <br>
-                <input type="text" class="form-text-input" name="lName" id="input-lname" value="<?php echo htmlspecialchars($lName, ENT_QUOTES) ?>" placeholder="Last name*">
-                <?php if ($lNameNotice) {
-                    echo "<p class='notice'>" . $lNameNotice . "<p>";
-                }
-                ?>
-                <br>
-                <input type="text" class="form-text-input" name="login" id="input-login" value="<?php echo htmlspecialchars($login, ENT_QUOTES) ?>" placeholder="Login*">
-                <?php if ($loginNotice) {
-                    echo "<p class='notice'>" . $loginNotice . "<p>";
-                }
-                ?>
-                <aside><strong>Login must contain:</strong>
-                    <ul>
-                        <li>from 4 to 30 characters</li>
-                        <li>at least one letter</li>
-                        <li>at least one digit</li>
-                    </ul>
-                </aside>
-                <input type="password" name="password" id="input-password" value="<?php echo htmlspecialchars($password, ENT_QUOTES) ?>" placeholder="Password*">
-                <?php if ($passwordNotice) {
-                    echo "<p class='notice'>" . $passwordNotice . "<p>";
-                }
-                ?>
-                <aside><strong>Password must contain:</strong>
-                    <ul>
-                        <li>from 8 to 30 characters</li>
-                        <li>at least one letter</li>
-                        <li>at least one digit</li>
-                    </ul>
-                </aside>
-                <input type="password" name="password2" id="input-password2" value="<?php echo htmlspecialchars($password2, ENT_QUOTES) ?>" placeholder="Confirm password*">
-                <?php if ($password2Notice) {
-                    echo "<p class='notice'>" . $password2Notice . "<p>";
-                }
-                ?>
-                <br>
-                <input type="submit" name="submit">
-            </form>
-        </section>
+            <h2>Please enter your login details</h2>
+            <?php
+            echo $fName->get_html();
+            echo $lName->get_html();
+            echo $login->get_html();
+            ?>
+            <aside><strong>Login must contain:</strong>
+                <ul>
+                    <li>from 4 to 30 characters</li>
+                    <li>at least one letter</li>
+                    <li>at least one digit</li>
+                </ul>
+            </aside>
+            <?php
+            echo $password->get_html();
+            ?>
+            <aside><strong>Password must contain:</strong>
+                <ul>
+                    <li>from 8 to 30 characters</li>
+                    <li>at least one letter</li>
+                    <li>at least one digit</li>
+                </ul>
+            </aside>
+            <?php
+            echo $password2->get_html();
+            ?>
+            <input type="submit" name="submit" value="Register" title="Register">
+        </form>
     </main>
 </body>
 
 
 </html>
-<!-- <?php
-        echo '
-<pre>';
-        print_r($_POST);
-        echo '</pre>';
-
-        ?> -->
--- script.js
+++ script.js
@@ -8,9 +8,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     /* login */
     // TODO move to its own JS file
-    const username_field = document.getElementById("input-login");
+    const username_field = document.querySelector(`
+    #form-register input[name="login"]`);
     if (username_field) {
-        username_field.addEventListener("input", usernameExists)
+        username_field.addEventListener("input", validateField)
+    }
+    const password_field = document.querySelectorAll(`input[type="password"]`);
+    if (password_field) {
+        password_field[0].addEventListener("input", validateField)
+        password_field[1].addEventListener("input", validateField)
     }
 });
 function showClear(e) {
@@ -25,13 +31,85 @@ function clearSearch(e) {
     e.currentTarget.children[0].classList.add("hidden");
 }
 
-async function usernameExists(e) {
-    const response = await fetch(`https://zwa.toad.cz/~husarma1/users.php?login=${e.target.value}`);
-    const exists = await response.json();
+async function validateField(e) {
+    const noticeElem = e.target.nextElementSibling;
+    let notice, isValid;
+    const name = e.target.placeholder.slice(0, -1);
+    if (e.target.value) {
+        if (e.target.name != "password2") {
+            [notice, isValid] = validate_input(e.target.value, e.target.minLength, e.target.maxLength, name);
+            if (isValid) {
+                if (e.target.name == "login") {
+                    const response = await fetch(`https://zwa.toad.cz/~husarma1/users.php?login=${e.target.value}`);
+                    const exists = await response.json();
+                    if (exists) {
+                        isValid = false;
+                        notice = "Username already in use."
+                    } else {
+                        notice = "Username available.";
+                    }
+                } else {
+                    const password_field = document.querySelector(`input[name="password2"]`);
+                    password_field.dispatchEvent(new Event("input"));
+                }
+            }
+        } else {
+            const password_field = document.querySelector(`input[type="password"]`);
+            if (password_field.value == e.target.value) {
+                isValid = true;
+                notice = "Passwords match";
+            } else {
+                isValid = false;
+                notice = "Passwords do not match";
+            }
+        }
 
-    if (exists) {
-        e.target.style.backgroundColor = "red";
+
+
+        if (isValid) {
+            e.target.classList.remove("incorrectInput");
+            e.target.classList.add("correctInput");
+            // e.target.classList.remove("incorrectText");
+            // e.target.classList.add("correctText");
+            noticeElem.classList.remove("incorrectText");
+            noticeElem.classList.add("correctText");
+            noticeElem.innerHTML = notice;
+        } else {
+            e.target.classList.add("incorrectInput");
+            e.target.classList.remove("correctInput");
+            // e.target.classList.add("incorrectText");
+            // e.target.classList.remove("correctText");
+            noticeElem.classList.add("incorrectText");
+            noticeElem.classList.remove("correctText");
+            noticeElem.innerHTML = notice;
+        }
     } else {
-        e.target.style.backgroundColor = "green";
+        e.target.classList.remove("incorrectInput");
+        e.target.classList.remove("correctInput");
+        noticeElem.innerHTML = "";
     }
+}
+
+
+function validate_input(input, minLength, maxLength, name = "") {
+    // Check length
+    length = input.length;
+    if (length < minLength) {
+        return [name + " must be longer than " + minLength + " characters.", false];
+    }
+    if (length > maxLength) {
+        return [name + " must be shorter than " + maxLength + " characters.", false];
+    }
+
+    // Check for at least one letter and one digit using regular expressions
+    if (!input.match(/[A-Za-z]/)) {
+        return [name + " must contain at least one letter.", false];
+    }
+
+    if (!input.match(/[0-9]/)) {
+        return [name + " must contain at least one digit.", false];
+    }
+
+    // If all conditions are met, the password is valid
+    return [name + " is valid.", true];
 }
